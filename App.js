e03ab1a97cc425bdeadaf566e749f58b4b7aacfe
Yoke Yue <39672956+yokester98@users.noreply.github.com>
2021-05-20 16:54:20 +0800

    First suggested template of the app

---
+++ App.js
@@ -0,0 +1,160 @@
+import "react-native-gesture-handler";
+
+import * as React from "react";
+
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from "react-native";
+
+import HomeScreen from "./app/screens/HomeScreen";
+import ProfileScreen from "./app/screens/ProfileScreen";
+import ForumScreen from "./app/screens/ForumScreen";
+import ReviewScreen from "./app/screens/ReviewScreen";
+import UploadScreen from "./app/screens/UploadScreen";
+// import SettingsScreen from "./app/screens/SettingsScreen";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// Logo title for the top left of every screen
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 100, height: 100 }}
+      source={require("./app/assets/yeppeo_title.png")}
+    />
+  );
+}
+
+function HomeStack() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function ReviewStack() {
+  return (
+    <Stack.Navigator initialRouteName="Review">
+      <Stack.Screen
+        name="Review"
+        component={ReviewScreen}
+        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function UploadStack() {
+  return (
+    <Stack.Navigator initialRouteName="Upload">
+      <Stack.Screen
+        name="Upload"
+        component={UploadScreen}
+        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function ForumStack() {
+  return (
+    <Stack.Navigator initialRouteName="Forum">
+      <Stack.Screen
+        name="Forum"
+        component={ForumScreen}
+        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function ProfileStack() {
+  return (
+    <Stack.Navigator initialRouteName="Profile">
+      <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        tabBarOptions={{
+          activeTintColor: "#ff0000",
+        }}
+      >
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ReviewStack"
+          component={ReviewStack}
+          options={{
+            tabBarLabel: "Review",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="rate-review" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="UploadStack"
+          component={UploadStack}
+          options={{
+            tabBarLabel: "Upload",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="upload" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ForumStack"
+          component={ForumStack}
+          options={{
+            tabBarLabel: "Forum",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="forum" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ProfileStack"
+          component={ProfileStack}
+          options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+} from "react-native";
+
+const HomeScreen = ({ route, navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: "center",
+              marginBottom: 16,
+            }}
+          >
+            You are on Home Screen
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    width: 300,
+    marginTop: 16,
+  },
+  title: {
+    justifyContent: "center",
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 0,
+    left: 10,
+  },
+});
+
+export default HomeScreen;
